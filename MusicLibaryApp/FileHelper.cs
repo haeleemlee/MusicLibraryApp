8cc0a1ca5b5fc6eab6512bbfa5dfc66fd4de661c
sunny5692413 <54248954+sunny5692413@users.noreply.github.com>
2019-08-25 18:43:38 -0700

    Format FileHelper.cs

--- MusicLibaryApp/FileHelper.cs
+++ MusicLibaryApp/FileHelper.cs
@@ -11,15 +11,15 @@ namespace MusicLibaryApp
         public async static void WriteTextFileAsync(string filename, string content)
         {
             var storageFolder = ApplicationData.Current.LocalFolder;
-            var textFile = await storageFolder.CreateFileAsync(filename,  CreationCollisionOption.OpenIfExists);
-           // var textStream = await textFile.OpenAsync(FileAccessMode.ReadWrite);
-           // var textWriter = new DataWriter(textStream);
-           // textWriter.WriteString(content);
+            var textFile = await storageFolder.CreateFileAsync(filename, CreationCollisionOption.OpenIfExists);
+            // var textStream = await textFile.OpenAsync(FileAccessMode.ReadWrite);
+            // var textWriter = new DataWriter(textStream);
+            // textWriter.WriteString(content);
             File.AppendAllLines(textFile.Path, new string[] { content });
 
-           // await textWriter.StoreAsync();
+            // await textWriter.StoreAsync();
         }
-        public async static Task<string> ReadTextFileAsync (string filename)
+        public async static Task<string> ReadTextFileAsync(string filename)
         {
             var storageFolder = ApplicationData.Current.LocalFolder;
             var textFile = await storageFolder.GetFileAsync(filename);
